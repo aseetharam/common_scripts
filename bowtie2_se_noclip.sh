e391cf748616902df2d35078fd5cf0876a2f2fa5
Arun Seetharam <arnstrm@iastate.edu>
2015-01-30 09:29:55 -0600

    deleted script

--- bowtie2_se_noclip.sh
+++ bowtie2_se_noclip.sh
@@ -5,6 +5,6 @@ module load bowtie2
 INPUT="$1"
 OUTPUT=$(basename ${INPUT})
 GENOME="/home/arnstrm/arnstrm/GMAPDB/Lvannamei_ray_scaffolds"
-bowtie2 --un-gz ${OUTPUT%%.*}_unaligned.sam.gz --threads 8 -x ${GENOME} -U ${INPUT} -S ${OUTPUT%%.*}.sam
+bowtie2 --un-gz ${OUTPUT%%.*}_unaligned.fq.gz --threads 8 -x ${GENOME} -U ${INPUT} -S ${OUTPUT%%.*}.sam
 samtools view -bS ${OUTPUT%%.*}.sam > ${OUTPUT%%.*}.bam
 samtools sort ${OUTPUT%%.*}.bam ${OUTPUT%%.*}_sorted
